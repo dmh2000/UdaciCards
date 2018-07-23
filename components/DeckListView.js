0079fdfac609a43ee7d4dc017b2a1d4973c912a4
david howard <dmh2000@gmail.com>
2018-07-23 10:23:48 -0700

    initial async storage and animation added

--- components/DeckListView.js
+++ components/DeckListView.js
@@ -1,14 +1,82 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  View ,
+  TouchableOpacity, 
+  Dimensions,
+  Animated
+} from 'react-native';
 
-import {createId} from '../utils/api.js';
+import TextButton from './TextButton';
+import {createId,getDecks} from '../utils/api.js';
+import {black,gray} from '../utils/colors';
 
 export default class DeckListView extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>DeckListView {createId('home')}</Text>
+  state = {
+    decks: {},
+    bounceValue: new Animated.Value(0)
+  }
+
+  componentDidMount() {
+    getDecks()
+      .then( (decks) => {
+        this.setState(() => ({
+          decks
+        }));
+      });
+  }
+  
+  onPress(deckName) {
+    return () => {
+      // animation
+      const {bounceValue} = this.state;
+      Animated.sequence([
+        Animated.timing(bounceValue, {duration:250,toValue:1}),
+        Animated.spring(bounceValue, {toValue:1}),
+      ]).start( () => {
+        // navigate to specified deck
+        console.log('start');
+        this.setState({bounceValue:new Animated.Value(0)});
+        this.props.navigation.navigate('DeckView', {deckName:deckName})
+      });
+
+
+    }
+  }
+
+  renderDeckList(decks) {
+    if (!decks) {
+      return;
+    }
+    return Object.keys(decks).map( (deck) => {
+
+      // extract the parameters needed
+      const len = decks[deck].questions.length;
+      const cards =  len === 1 
+        ? '1 card'
+        : `${len} cards`;
+      const title = decks[deck].title;
+      const {width} = Dimensions.get('window');
+      
+      // render a pressable view
+      return (
+      <TouchableOpacity key={title} onPress={this.onPress(title)}>
+      <View  style={[styles.card,{width:width-40}]}>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.count}>{cards}</Text>
       </View>
+      </TouchableOpacity>
+      )
+    })
+  }
+  
+  render() {
+    console.log('render',this.state.bounceValue);
+    return (
+      <Animated.View style={[styles.container,{transform: [{scaleX:this.state.bounceValue}]}]}>
+        {this.renderDeckList(this.state.decks)}
+      </Animated.View>
     );
   }
 }
@@ -20,4 +88,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  card: {
+    alignItems: 'center',
+    borderBottomColor: black,
+    borderBottomWidth: 1,
+    paddingTop: 40,
+    paddingBottom: 40,
+    marginTop: 20,
+    marginBottom:20
+  },
+  title: {
+    fontSize: 30
+  },
+  count: {
+    fontSize: 20,
+    color:gray
+  }
 });
--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,52 @@
 import {AsyncStorage} from 'react-native';
 
-export function getDecks() {
+export const DECK_STORAGE_KEY = 'UdaciCards:desks';
 
+const dummy = {
+  React: {
+    title: 'React',
+    questions: [
+      {
+        question: 'What is React?',
+        answer: 'A library for managing user interfaces'
+      },
+      {
+        question: 'Where do you make Ajax requests in React?',
+        answer: 'The componentDidMount lifecycle event'
+      }
+    ]
+  },
+  JavaScript: {
+    title: 'JavaScript',
+    questions: [
+      {
+        question: 'What is a closure?',
+        answer: 'The combination of a function and the lexical environment within which that function was declared.'
+      }
+    ]
+  }
+};
+
+export function getDecks() {
+  return AsyncStorage.getItem(DECK_STORAGE_KEY)
+  .then( (results) => {
+    // no decks yet
+    if (results === null) {
+      return AsyncStorage.setItem(DECK_STORAGE_KEY,JSON.stringify({}))
+      .then( () => {
+        return {};
+      });
+    }
+    else {
+      return dummy;
+    }
+  })
+  .then( (results) => {
+    return results;
+  })
+  .catch( (error) => {
+    return null;
+  })
 }
 
 export function getDeck(id) {
